5aa479b44671805e443f85c4da1d65286bb7f81e
Le Thanh Nam <lethanhnam330@gmail.com>
2023-11-30 11:47:51 +0700

    edit code

--- app/middlewares/server.js
+++ app/middlewares/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const path = require('path');
 
 //Cookie parser
 const cookie = (app) => {
@@ -38,6 +39,18 @@ const morganMethod = (app) => {
     app.use(morgan('common'));
 };
 
-const serverMiddleware = [cookie, corsPolicy, jsonParser, postHtml, helmetMethod, morganMethod];
+//public/images
+const pathImages = (app) => {
+    app.use('/images', express.static(path.join(pathDirname, 'public/images')));
+};
+
+const serverMiddleware = [
+    cookie,
+    corsPolicy,
+    jsonParser,
+    postHtml,
+    helmetMethod,
+    morganMethod,
+];
 
 module.exports = serverMiddleware;
--- server.js
+++ server.js
@@ -5,6 +5,26 @@ const dotenv = require('dotenv');
 const routes = require('./routes');
 const connect_db = require('./app/connect_db');
 const serverMiddleware = require('./app/middlewares/server');
+const path = require('path');
+
+//...
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    },
+});
+const upload = multer({ storage });
+app.post('/api/upload', upload.single('file'), function (req, res) {
+    return res.status(200).json('success');
+});
+//...
+
+//path Images
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
 
 //Middleware server
 serverMiddleware.map((x) => x(app));
