211e2ce716739c271c4fa1ad618cce236ca74e7d
Le Thanh Nam <lethanhnam330@gmail.com>
2024-01-03 09:00:51 +0700

    clean code

--- src/utils/actions/actions.ts
+++ src/utils/actions/actions.ts
@@ -5,33 +5,6 @@ import { getServerSession } from 'next-auth/next';
 import { revalidateTag } from 'next/cache';
 import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
-export const handleLikeTrackAction = async (id: any, quantity: any) => {
-    const session = await getServerSession(authOptions);
-
-    const likeAction = await sendRequest<IBackendRes<any>>({
-        url: `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/likes`,
-        method: 'POST',
-        body: {
-            track: id,
-            quantity,
-        },
-        headers: {
-            Authorization: `Bearer ${session?.access_token}`,
-        },
-    })
-        .then(async (res) => {
-            return res;
-        })
-        .catch((error) => {
-            console.log(error);
-            return error;
-        });
-
-    revalidateTag('track');
-
-    return likeAction;
-};
-
 export const handleFollowOrUnfollowUserAction = async (
     userIdFollowed: string,
     follow: boolean,
